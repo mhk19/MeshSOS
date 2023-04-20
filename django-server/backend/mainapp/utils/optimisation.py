08f7f895e89fcd61ef4e53dcb8cea24e0745187b
Yatin <yatinkhanna2000@gmail.com>
2023-04-20 07:45:08 +0530

    API for full routes

--- django-server/backend/mainapp/utils/optimisation.py
+++ django-server/backend/mainapp/utils/optimisation.py
@@ -2,6 +2,36 @@ from .create_zones import getZoneLocations, ZONE_LOC, AMB_LOC
 from .tabu_search import tabuSearch
 from .distance import euclidean
 from scipy.optimize import linear_sum_assignment
+from datetime import datetime
+
+# locationAndTimeData -> list of tuples of form (latitude, longitude, timestamp), timestamp format: "%Y-%m-%d %H:%M:%S"
+# return 2D list where list[i][j] -> the location of the ith vehicle for the jth period in the day
+# len(list) = numVehicles | len(list[i]) = 4 for every i   
+
+def getVehicleRoutes(locationAndTimeData, numVehicles):
+    # Morning -> (6 AM - 12 noon)
+    # Afternoon -> (12 noon - 6 PM)
+    # Evening -> (6 PM - 12 midnight)
+    # Night -> (12 midnight - 6 AM)
+    emergencyLocationLists = [list() for i in range(4)]   # List of 4 empty lists
+    for data in locationAndTimeData:
+        ts = datetime.strptime(data[2], "%Y-%m-%d %H:%M:%S")
+        hour = int(ts.hour)
+        emergencyLocationLists[hour // 6].append((float(data[0]), float(data[1])))
+
+    ambulanceLocationLists = [getVehicleLocations(emergencyLocationLists[i], numVehicles) for i in range(4)]
+
+    ambulanceRoutes = [[ambulanceLocationLists[0][i]] for i in range(numVehicles)]
+
+    for i in range(1, 4):
+        matches, maxTravel = getRoute(ambulanceLocationLists[i-1], ambulanceLocationLists[i])
+        for j in range(numVehicles):
+            for k in range(len(matches)):
+                if ambulanceRoutes[j][-1] == matches[k][0]:
+                    ambulanceRoutes[j].append(matches[k][1])
+                    break
+    
+    return ambulanceRoutes
 
 # emergencyLocations -> list of tuples of form (latitude, longitude), numVehicles -> number of available vehicles
 def getVehicleLocations(emergencyLocations, numVehicles):
